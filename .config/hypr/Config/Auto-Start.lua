477415ce3243ce74a15f95f0e6aacc5037b91961
cipher-iso <c.42415348494945@gmail.com>
2026-07-25 14:45:02 -0400

    Auto-sync dotfiles

--- .config/hypr/Config/Auto-Start.lua
+++ .config/hypr/Config/Auto-Start.lua
@@ -9,7 +9,7 @@ hl.exec_cmd(Polkit)	-- Authentication Agent
 
 -- [ GUI STARTUP ]
 --hl.exec_cmd(OBS)		-- OBS
-hl.exec_cmd("LD_PRELOAD=/usr/lib32/libextest.so steam")            -- Steam
+hl.exec_cmd("steam")            -- Steam
 hl.exec_cmd(Discord)            -- Discord
 hl.exec_cmd(Browser)            -- Browser
 hl.exec_cmd(EasyEffects)	-- Mic Equalizer
@@ -17,7 +17,7 @@ hl.exec_cmd(EasyEffects)	-- Mic Equalizer
 -- [ BACKGROUND STARTUP ]
 hl.exec_cmd(Listener)                                   -- Color-Schemes
 hl.exec_cmd(Sunshine)                                   -- Remote-Desktop
-hl.exec_cmd("waypaper --random")                       -- Wallpaper Restoration
+hl.exec_cmd("waypaper --random")                 	-- Wallpaper Restoration
 hl.exec_cmd("hyprctl eval 'ReloadSystem()'")		-- Reload System Functions
 hl.exec_cmd("wl-clip-persist --clipboard regular")	-- Persist Clipboard History
 hl.exec_cmd("hyprctl eval 'Screenshare_Restore()'")	-- Restore ScreenShare State
--- .config/hypr/Config/Environment.lua
+++ .config/hypr/Config/Environment.lua
@@ -8,23 +8,23 @@ hl.env("PATH", (os.getenv("HOME") or "") .. "/Scripts:" .. (os.getenv("PATH") or
 hl.env("PULSE_LATENCY_MSEC", "60")
 
 -- CURSOR SETTINGS 	[ THEME & SIZE ]
-hl.env("XCURSOR_SIZE",                "24")
-hl.env("HYPRCURSOR_SIZE",             "24")
-hl.env("XCURSOR_THEME",               "Bibata-Modern-Ice")
-hl.env("HYPRCURSOR_THEME",            "Bibata-Modern-Ice")
+hl.env("XCURSOR_SIZE",		"24")
+hl.env("HYPRCURSOR_SIZE",	"24")
+hl.env("XCURSOR_THEME",		"Bibata-Modern-Ice")
+hl.env("HYPRCURSOR_THEME",	"Bibata-Modern-Ice")
 
 -- THEME SETTINGS 	[ GTK & QT ]
-hl.env("GTK_THEME",                           "matrix")
-hl.env("QT_QPA_PLATFORMTHEME",                "qt6ct")
-hl.env("QT_QPA_PLATFORM",                     "wayland;xcb")
-hl.env("QT_AUTO_SCREEN_SCALE_FACTOR",         "1")
-hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("GTK_THEME",                           	"matrix")
+hl.env("QT_QPA_PLATFORMTHEME",                	"qt6ct")
+hl.env("QT_QPA_PLATFORM",                     	"wayland;xcb")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR",         	"1")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION",	"1")
 
 -- MENU SETTINGS 	[ HYPRLAND-SPECIFIC ]
-hl.env("XDG_MENU_PREFIX",      "arch-")
-hl.env("XDG_SESSION_TYPE",     "wayland")
-hl.env("XDG_CURRENT_DESKTOP",  "Hyprland")
-hl.env("XDG_SESSION_DESKTOP",  "Hyprland")
+hl.env("XDG_MENU_PREFIX",	"arch-")
+hl.env("XDG_SESSION_TYPE",     	"wayland")
+hl.env("XDG_CURRENT_DESKTOP",  	"Hyprland")
+hl.env("XDG_SESSION_DESKTOP",  	"Hyprland")
 
 -- NVIDIA SETTINGS 	[ PERFORMANCE & OPTIMIZATIONS ]
 local handle = io.popen("lspci 2>/dev/null | grep -i nvidia")
